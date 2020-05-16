3fe6844993e2ef239215d8de7710a0e9f5b0407b
rochfolly <rochfolly@hotmail.fr>
2020-05-16 13:48:45 -0400

    (feat) expense modal view

---
+++ src/style/modalStyle.js
@@ -0,0 +1,36 @@
+export default {
+    // New Expense Button
+    modalView: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: 'rgb(240, 240, 240)',
+      width: '90%',
+      marginTop: '20%',
+      marginBottom: '20%',
+      marginRight: '5%',
+      marginLeft: '5%'
+  },
+    // New Expense Button
+    editButton: {
+        backgroundColor: 'green',
+        padding: 5,
+        height: 30, 
+        width: 200,
+        alignItems: 'center',
+       // borderRadius: '10',
+    },
+    // New Expense Button
+    removeButton: {
+        backgroundColor: 'red',
+        padding: 3,
+       // height: 30, 
+        width: 90,
+        alignItems: 'center',
+       // borderRadius: '10',
+    },
+    closeIcon: {
+        position: 'relative',
+        bottom: 0,
+        top: 0
+    }
+  };
